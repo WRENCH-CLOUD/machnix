dd0056faaf4c00f9eac6e4de9f28ae638766887f
Rohithgg <khanarohithfi@gmail.com>
2026-01-13 12:41:21 +0530

    refactor: remove 'Manage Subscription' option from tenant dropdown menu

--- src/app/(admin)/tenants/page.tsx
+++ src/app/(admin)/tenants/page.tsx
@@ -377,9 +377,6 @@ export default function TenantsPage() {
                               <LogIn className="w-4 h-4 mr-2" />
                               Login as Tenant (Coming Soon)
                             </DropdownMenuItem>
-                            <DropdownMenuItem>
-                              Manage Subscription
-                            </DropdownMenuItem>
                             <DropdownMenuItem
                               onClick={() => handleDeleteTenant(tenant)}
                               className="text-destructive focus:text-destructive"
