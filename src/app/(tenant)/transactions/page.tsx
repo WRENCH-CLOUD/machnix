00dbf698e1f074d388c5e760b4bce45f6d01c092
sagun-py0909 <sagunvarma25@gmail.com>
2026-02-06 01:40:09 +0530

    transactions_page_update

--- src/app/(tenant)/transactions/page.tsx
+++ src/app/(tenant)/transactions/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { useTransactions } from "@/hooks/queries";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import { Badge } from "@/components/ui/badge";
 import {
     Table,
